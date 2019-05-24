8c8eb91f7b98803dea04474b4ca0491ef54a27cf
govindjsk <govindsharmajsk@gmail.com>
2019-05-24 13:16:23 +0530

    Added class Hypergraph and its corresponding test file.

---
+++ src/hypergraph.py
@@ -0,0 +1,17 @@
+
+class Hypergraph:
+    def __init__(self, vertices=None, hyperedges=None):
+        self.V = vertices
+        self.F = hyperedges
+        self.resolve_E()
+
+    def resolve_E(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
