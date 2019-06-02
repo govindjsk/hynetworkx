5816bae9929bfa563a1b88b9e3c85328904fa40f
govindjsk <govindsharmajsk@gmail.com>
2019-06-03 03:39:10 +0530

    Enhanced the hyperedge class.

---
+++ tests/test_hypergraph.py
@@ -0,0 +1,31 @@
+from src.hypergraph import Hypergraph, generate_hypergraph
+from src.vertex_set import generate_monotonic_vertex_set
+
+
+def test_init_empty_hypergraph():
+    H = Hypergraph()
+    print(H.__summary__)
+    assert len(H.V) == 0 and len(H.F) == 0
+
+
+def test_init_semiempty_hypergraph():
+    size = 5
+    V = generate_monotonic_vertex_set(size)
+    H = Hypergraph(V)
+    print(H.__summary__)
+    assert len(H.V) == size and len(H.F) == 0
+
+
+def test_init_nonempty_hypergraph():
+    num_vertices = 5
+    num_hyperedges = 10
+    H = generate_hypergraph(num_vertices, num_hyperedges)
+    print(H.__summary__)
+    assert len(H.V) == num_vertices and len(H.F) == num_hyperedges
+
+
+def test_incidence_matrix():
+    H = generate_hypergraph(110, 10)
+    print(H.__summary__)
+    H.S.pprint(True)
+    assert H.S.shape == (110, 10)
