84801694b6d8f53e393ca31a24277f915d547e32
govindjsk <govindsharmajsk@gmail.com>
2019-07-08 09:47:49 +0530

    Moved from np.ndarray to scipy.sparse.csr_matrix

--- src/incidence_matrix.py
+++ src/incidence_matrix.py
@@ -1,27 +1,30 @@
 import numpy as np
-from src.utils import print_matrix
+from src.utils import print_matrix, _or
 from src.vertex import Vertex
+from scipy.sparse import csr_matrix
 
 
-class IncidenceMatrix(np.ndarray):
-    def __new__(cls, matrix=None, vertex_list=None):
-        matrix = matrix or np.array([])
-        obj = np.asarray(matrix).view(cls)
-        obj.vertex_list = vertex_list
-        if len(obj.shape) == 1:
-            if obj.shape[0] == 0:
-                obj = obj.reshape(0, 0)
-            else:
-                obj = obj.reshape(obj.shape[0], 1)
-        return obj
+class IncidenceMatrix(csr_matrix):
+    # def __new__(cls, matrix=None, vertex_list=None):
+    #     matrix = _or(matrix, csr_matrix, ([],))
+    #     obj = csr_matrix(matrix).view(cls)
+    #     obj.vertex_list = vertex_list
+    #     if len(obj.shape) == 1:
+    #         if obj.shape[0] == 0:
+    #             obj = obj.reshape(0, 0)
+    #         else:
+    #             obj = obj.reshape(obj.shape[0], 1)
+    #     return obj
 
-    # def __init__(self, matrix, vertex_list):
-    #     super(IncidenceMatrix, self).__init__(matrix)
+    # def __array_finalize__(self, obj):
+    #     if obj is None:
+    #         return
+    #     self.vertex_list = getattr(obj, 'vertex_list', None)
 
-    def __array_finalize__(self, obj):
-        if obj is None:
-            return
-        self.vertex_list = getattr(obj, 'vertex_list', None)
+    def __init__(self, matrix=None, vertex_list=None):
+        matrix = _or(matrix, csr_matrix, ([],))
+        self.vertex_list = vertex_list
+        super(IncidenceMatrix, self).__init__(matrix)
 
     def pprint(self, show_labels=False):
         if show_labels:
