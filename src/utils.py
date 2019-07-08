a0f0276c70634b1dcd60071ccf3b29dc663b60a2
govindjsk <govindsharmajsk@gmail.com>
2019-07-08 09:49:29 +0530

    Modified the logic to print csr_matrix.

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 def print_matrix(A, row_labels=None):
+    A = A.toarray()
     row_labels = row_labels or ['' for i in range(A.shape[0])]
     print()
     if A.ndim == 1:
@@ -14,8 +15,15 @@ def print_matrix(A, row_labels=None):
             for i, AAA in enumerate(AA[:-1]):
                 w1 = max([len(str(s)) for s in A[:, i]])
                 print(str(AAA) + ' ' * (w1 - len(str(AAA)) + 1), end='')
-            w1 = max([len(str(s)) for s in A[:, -1]])
+            w1 = max([len(str(s)) for s in A[:, -1]]) if A.shape[0]*A.shape[1] > 0 else 1
             print(str(AA[-1]) + ' ' * (w1 - len(str(AA[-1]))), end='')
             print(' ', end='')
             print(u'\u2502')
         print(' ' * label_w + u'\u2514' + u'\u2500' * w + u'\u2518')
+
+
+def _or(item, generator, args):
+    if isinstance(item, type(None)):
+        return generator(*args)
+    else:
+        return item
