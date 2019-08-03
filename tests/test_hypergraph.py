aa3b1172171d77af931fa5425480112907241944
govindjsk <govindsharmajsk@gmail.com>
2019-08-03 23:16:02 +0530

    Now inducing graph from the Hypergraph class. A new class, Graph with a method to convert it to networkx format.

--- tests/test_hypergraph.py
+++ tests/test_hypergraph.py
@@ -1,4 +1,4 @@
-from src.hypergraph import Hypergraph, generate_hypergraph, parse_benson_hypergraph
+from src.hypergraph import Hypergraph, generate_hypergraph, parse_benson_hypergraph, Graph, generate_graph
 from src.vertex_set import generate_monotonic_vertex_set
 import pytest
 
@@ -47,4 +47,57 @@ def test_parse_benson_hypergraph(name, nV, nF):
     H = parse_benson_hypergraph(name)
     print(H.S.shape)
     assert max(H.V)[0] + 1 == nV and len(H.F) == nF and \
-           H.S.shape == (nV, nF) and H.A.shape == (nV, nV)
+        H.S.shape == (nV, nF) and H.A.shape == (nV, nV)
+
+
+def test_graph_empty():
+    G = Graph()
+    print(G.__summary__)
+    print(G)
+    assert len(G.V) == 0 and len(G.E) == 0
+
+
+def test_init_semiempty_graph():
+    size = 5
+    V = generate_monotonic_vertex_set(size)
+    G = Graph(V)
+    print(G.__summary__)
+    assert len(G.V) == size and len(G.E) == 0
+
+
+def test_init_nonempty_graph():
+    num_vertices = 5
+    num_edges = 10
+    G = generate_graph(num_vertices, num_edges)
+    print(G.__summary__)
+    print(G)
+    assert len(G.V) == num_vertices and len(G.E) == num_edges
+
+
+def test_induce_graph():
+    H = generate_hypergraph(5, 2, (3, 5))
+    print(H.__summary__)
+    assert not H._G_computed
+    G = H.G
+    assert H._G_computed
+    print(G.__summary__)
+    assert isinstance(G, Graph)
+    assert G.V == H.V and all([any([e.issubset(f) for f in H.F]) for e in G.E]) and H.E == G.E
+
+
+@pytest.mark.parametrize('name, nV, nF', [
+                                        ('contact-high-school', 327, 489),
+                                        ('contact-primary-school', 242, 434),
+                                        ('email-Enron', 148, 114),
+                                         ])
+def test_parse_benson_hypergraph_graph(name, nV, nF):
+    H = parse_benson_hypergraph(name)
+    G = H.G
+    print(G)
+    assert G.V == H.V and H.E == G.E
+
+
+def test_induced_graph_nx():
+    H = generate_hypergraph(5, 2, (3, 5))
+    print(H.__summary__)
+    G = H.G.to_nx()
