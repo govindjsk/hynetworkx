a9a3f0aab48a58d8554e0357aee48e2668a991e0
govindjsk <govindsharmajsk@gmail.com>
2019-08-03 23:13:46 +0530

    Added an EdgeSet class and its unit test cases.

--- src/hyperedge_set.py
+++ src/hyperedge_set.py
@@ -1,5 +1,6 @@
-from .hyperedge import Hyperedge, generate_hyperedge, generate_hyperedge_from_vertices
 from src.utils import is_iterable
+from .hyperedge import Hyperedge, generate_hyperedge, generate_hyperedge_from_vertices, Edge, \
+    generate_edge_from_vertices
 
 HYPEREDGE_SET_DEFAULTS = {'count': 5}
 
@@ -15,9 +16,31 @@ class HyperedgeSet(set):
         # assert all([isinstance(f, Hyperedge) for f in hyperedges])
         super(HyperedgeSet, self).__init__(hyperedges)
 
+    def __repr__(self):
+        return 'HyperedgeSet({})'.format(',\n\t\t'.join(map(str, sorted(self))))
+
     @property
     def __summary__(self):
         return '{' + ', '.join([f.__summary__ for f in sorted(self, key=lambda g: (len(g), tuple(sorted(g))))]) + '}'
+
+
+class EdgeSet(set):
+    def __init__(self, edges=None):
+        edges = edges or ()
+        assert is_iterable(edges), "Edges not iterable: {}".format(edges)
+        if not all([isinstance(e, Edge) for e in edges]):
+            edges = {Edge(e) for e in edges}
+        elif not isinstance(edges, set):
+            edges = set(edges)
+        # assert all([isinstance(f, Hyperedge) for f in hyperedges])
+        super(EdgeSet, self).__init__(edges)
+
+    @property
+    def __summary__(self):
+        return '{' + ', '.join([f.__summary__ for f in sorted(self, key=lambda g: (len(g), tuple(sorted(g))))]) + '}'
+
+    def __repr__(self):
+        return 'EdgeSet({})'.format(',\n\t\t'.join(map(str, sorted(self))))
 
 
 def generate_hyperedge_set(count=None,
@@ -53,6 +76,19 @@ def generate_hyperedge_set_from_vertices(vertices,
         reserved_hyperedges.add(f)
         F.add(f)
     return F
+
+
+def generate_edge_set_from_vertices(vertices,
+                                    count=None,
+                                    reserved_edges=None):
+    count = count or HYPEREDGE_SET_DEFAULTS['count']
+    reserved_edges = reserved_edges or set()
+    E = EdgeSet()
+    while len(E) < count:
+        e = generate_edge_from_vertices(vertices, reserved_edges)
+        reserved_edges.add(e)
+        E.add(e)
+    return E
 
 
 def main():
--- tests/test_hyperedge_set.py
+++ tests/test_hyperedge_set.py
@@ -1,7 +1,9 @@
 from pprint import pprint
 
-from src.hyperedge import Hyperedge, generate_hyperedge
-from src.hyperedge_set import HyperedgeSet, generate_hyperedge_set
+import pytest
+
+from src.hyperedge import Hyperedge, generate_hyperedge, Edge
+from src.hyperedge_set import HyperedgeSet, generate_hyperedge_set, EdgeSet
 
 
 def test_init_empty_hyperedge_set():
@@ -21,3 +23,36 @@ def test_init_():
     F = generate_hyperedge_set(count=count)
     pprint(F.__summary__)
     assert len(F) == count
+
+
+def test_edge_set_empty():
+    E = EdgeSet()
+    print(E.__summary__)
+    assert len(E) == 0
+
+
+@pytest.mark.parametrize('edges', [1,
+                                   (1, 2),
+                                   [tuple()],
+                                   [(1, 2, 3), (1, 2)]])
+def test_edge_set_invalid(edges):
+    print(edges)
+    with pytest.raises(AssertionError):
+        EdgeSet(edges)
+
+
+@pytest.mark.parametrize('edges', [[(1, 2)],
+                                   [(1,)],
+                                   [(1, 2), (1, 2)],
+                                   [Edge([1, 2])]])
+def test_edge_set_one(edges):
+    print(edges)
+    E = EdgeSet(edges)
+    print(E.__summary__)
+    assert len(E) == 1
+
+
+def test_edge_set_two():
+    E = EdgeSet([(1, 2), (2, 3)])
+    print(E.__summary__)
+    assert len(E) == 2
